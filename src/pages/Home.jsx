15fb6dc7981960b683fe168b25a760e10ea5ac60
Mrinal Pramanick <mrinalbitsat@gmail.com>
2024-05-26 02:02:46 +0530

    Added New data for features section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,40 +1,46 @@
-
 import React from "react";
-import { SparklesCore } from "../components/ui/sparkles";
+import {Spot} from "./Spot"
+import { SparklesCore } from "../componentsAce/ui/sparkles";
 import Reveal from "./Reveal";
 import { Features } from "./Features";
- 
+import { Rethink } from "./Rethink";
+import { Team } from "./Team";
+
+
 export function Home() {
   return (
     <>
-    <div className="h-screen w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-sm">
-      <h1 className="md:text-7xl text-3xl lg:text-9xl font-bold text-center text-white relative z-20">
-      Innovarium
-      </h1>
-      <div className="w-[40rem] h-40 relative">
-        {/* Gradients */}
-        <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm" />
-        <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4" />
-        <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[5px] w-1/4 blur-sm" />
-        <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px w-1/4" />
- 
-        {/* Core component */}
-        <SparklesCore
-          background="transparent"
-          minSize={0.4}
-          maxSize={1}
-          particleDensity={1200}
-          className="w-full h-full"
-          particleColor="#FFFFFF"
-        />
- 
-        {/* Radial Gradient to prevent sharp edges */}
-        <div className="absolute inset-0 w-full h-full bg-black [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
-      </div>
+      <div className="h-screen w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-sm">
+        <h1 className="md:text-7xl text-3xl lg:text-9xl font-bold text-center text-white relative z-20">
+          Innovarium
+        </h1>
+        <div className="w-[40rem] h-40 relative">
+          {/* Gradients */}
+          <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm" />
+          <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4" />
+          <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[5px] w-1/4 blur-sm" />
+          <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px w-1/4" />
 
-      <Reveal />
-    </div>
+          {/* Core component */}
+          <SparklesCore
+            background="transparent"
+            minSize={0.4}
+            maxSize={1}
+            particleDensity={1200}
+            className="w-full h-full"
+            particleColor="#FFFFFF"
+          />
+
+          {/* Radial Gradient to prevent sharp edges */}
+          <div className="absolute inset-0 w-full h-full bg-black [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
+        </div>
+
+        <Reveal />
+      </div>
       <Features />
+      <Rethink />
+      <Spot />
+      <Team />
     </>
   );
 }
--- src/pages/Reveal.jsx
+++ src/pages/Reveal.jsx
@@ -1,4 +1,4 @@
-import { TextGenerateEffect } from "../components/ui/text-generate-effect";
+import { TextGenerateEffect } from "../componentsAce/ui/text-generate-effect";
 
 const words = `Captures the essence of storing various researches and npm packages while emphasizing creativity and exploration`;
 
